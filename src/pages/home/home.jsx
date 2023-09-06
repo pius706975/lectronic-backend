d3b9524a9848fa7fb249f3ba5a1c343dfdfc5b26
pius <piusrestiantoro2@gmail.com>
2023-09-06 15:59:50 +0800

    [Fix] Router updated.

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from "react"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import './home.css'
+import navbar from '../../components/navbar/navbar'
+import footer from '../../components/footer/footer'
 
 function Home() {
     
@@ -11,6 +13,8 @@ function Home() {
 
     return (
         <div className="home-app">
+            <navbar/>
+            <footer/>
         </div>
     )
 }
--- src/routers.js
+++ src/routers.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from "./pages/home/home"
+import Login from './pages/login/login'
 
 function TheRouter() {
     return (
@@ -9,6 +10,7 @@ function TheRouter() {
             <Routes>
 
                 <Route path="/" element={<Home/>} />
+                <Route path="/login" element={<Login/>} />
 
             </Routes>
 
